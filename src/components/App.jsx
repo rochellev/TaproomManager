6b05cc2e09f90262aff1e52be697efa6d898bf11
rochellev <rochellevroberts@utexas.edu>
2019-09-09 16:24:31 -0700

    add kew keg form and control

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from './Header';
 import Error404 from './Error404';
 import { Switch, Route } from 'react-router-dom';
 import FilterableKegTable from './FilterableKegTable';
+import NewKegControl from './NewKegControl';
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
             <Header />
             <Switch>
                 <Route exact path='/' component={FilterableKegTable} />
+                < Route path='/newkeg' component={NewKegControl} />
                 <Route component={Error404} />
             </Switch>
         </div>
---
+++ src/components/NewKegForm.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function NewKegForm(props){
+    let _name = null;
+    let _brand = null;
+    let _price = null;
+    let _alcoholContent = null;
+    
+}
+
+
+export default NewKegForm;
