a1de9b08e84cce3edb5ff637897df119829d4452
rochellev <rochellevroberts@utexas.edu>
2019-09-13 13:37:52 -0700

    add card

--- src/components/Keg.jsx
+++ src/components/Keg.jsx
@@ -2,18 +2,27 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 function Keg(props) {
+  
   const kegInformation =
-    <div>
-      <li>Name: {props.name}</li>
-      <li>Brand: {props.brand}</li>
-      <li>Price: ${props.price}</li>
-      <li>Alcohol Content: {props.alcoholContent}%</li>
-      <li>Pints Remaining: {props.pintsRemaining}</li>
-      <br></br>
+    <div class='container'>
+    <div class='card border'>
+      <div class='card-header'>
+        <h4 class='card-title'>{props.name}</h4>
+        <h6 class='card-subtitle'>{props.brand}</h6>
+      </div>
+      <div class='card-body'>
+        <ul class='list-group list-group-flush'>
+          <li class='list-group-item'>Price: ${props.price}</li>
+          <li class='list-group-item'>Alcohol Content: {props.alcoholContent}%</li>
+          <li class='list-group-item'>Pints Remaining: {props.pintsRemaining}</li>
+        </ul>
+      </div>
+    </div>
+    <br></br>
     </div>;
   if (props.currentRouterPath === '/employee') {
     return (
-      <div onClick={() => {props.onKegSelection({name: props.name, brand: props.brand, price: props.price, alcoholContent: props.alcoholContent, pintsRemaining: props.pintsRemaining}); }}>
+      <div onClick={() => { props.onKegSelection({ name: props.name, brand: props.brand, price: props.price, alcoholContent: props.alcoholContent, pintsRemaining: props.pintsRemaining }); }}>
         {kegInformation}
       </div>
     );
