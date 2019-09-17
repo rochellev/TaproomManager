0162cd9f49bc3cbe7f438b89a7fb496f7b03961a
rochellev <rochellevroberts@utexas.edu>
2019-09-17 16:49:28 -0700

    add sample keg data

--- __tests__/reducers/keg-list-reducer.test.js
+++ __tests__/reducers/keg-list-reducer.test.js
@@ -1,5 +1,16 @@
+import kegListReducer from './../src/reducers/keg-list-reducer';
+
 describe('kegListReducer', ()=>{
-  test('test test: two plus tow is four', () => {
-    expect(2+2).toBe(4);
+
+  let action;
+  const sampleKegData = {
+    name: 'Coors Light',
+    brand: 'Company Brewer',
+    price: '3.00',
+    alcoholContent: '3.6',
+    pintsRemaining: '300'
+  }
+  test('Should return default state if no action type is recognized', () => {
+    expect(kegListReducer({}, {type: null})).toEqual({});
   });
 });
