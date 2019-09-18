672b88ad82dc366e1ad24feb028654ef0e13bd10
rochellev <rochellevroberts@utexas.edu>
2019-09-18 14:43:50 -0700

    remove master keg list from contructor state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -13,7 +13,6 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      masterKegList: {},
       selectedKeg: null
     };
     this.handleChangingSelectedKeg = this.handleChangingSelectedKeg.bind(this);
