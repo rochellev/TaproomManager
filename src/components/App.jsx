955dc433529c23e62bf390c524c98e501b6ec356
rochellev <rochellevroberts@utexas.edu>
2019-09-18 13:41:37 -0700

    remove handle new keg function

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,6 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import NewKegForm from './NewKegForm';
 import KegList from './KegList';
 import Employee from './Employee';
+import { v4 } from 'uuid';
 
 
 class App extends React.Component {
@@ -12,34 +13,26 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      masterKegList: [],
+      masterKegList: {},
       selectedKeg: null
     };
-    this.handleAddingNewKegToList = this.handleAddingNewKegToList.bind(this);
     this.handleChangingSelectedKeg = this.handleChangingSelectedKeg.bind(this);
   }
 
-  handleAddingNewKegToList(newKeg) {
-    var newMasterKegList = this.state.masterKegList.slice();
-    newMasterKegList.push(newKeg);
-    this.setState({ masterKegList: newMasterKegList });
-  }
-
-  handleChangingSelectedKeg(keg){
-    this.setState({selectedKeg: keg});
+  handleChangingSelectedKeg(kegID){
+    this.setState({selectedKeg: kegID});
   }
 
   render() {
     var appBackground = {
       backgroundColor: '#95A3B3',
     };
-
     return (
       <div style={appBackground}>
         <Header />
         <Switch>
           <Route exact path='/' render={() => <KegList kegList={this.state.masterKegList} />} />
-          <Route path='/newkeg' render={() => <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />} />
+          <Route path='/newkeg' render={() => <NewKegForm />} />
           <Route path='/employee' render={(props)=> <Employee kegList={this.state.masterKegList} currentRouterPath={props.location.pathname} onKegSelection={this.handleChangingSelectedKeg} selectedKeg={this.state.selectedKeg} />} />
           <Route component={Error404} />
         </Switch>
