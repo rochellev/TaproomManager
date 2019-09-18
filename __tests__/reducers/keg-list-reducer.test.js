b29ef0ff9d72065fd04bec5674d04beddbb19fc2
Student <example@example.com>
2019-09-18 08:44:22 -0700

    add test for adding new keg

--- __tests__/reducers/keg-list-reducer.test.js
+++ __tests__/reducers/keg-list-reducer.test.js
@@ -10,10 +10,33 @@ describe('kegListReducer', ()=>{
     alcoholContent: '3.6',
     pintsRemaining: '300',
     id: 0
-  }
+  };
+
   test('Should return default state if no action type is recognized', () => {
     expect(kegListReducer({}, {type: null})).toEqual({});
   });
 
-
+  test('Should successfully add new keg data to masterKegList', () => {
+    const {name, brand, price, alcoholContent, pintsRemaining, id} = sampleKegData;
+    action = {
+      type: 'ADD_KEG',
+      name: name,
+      brand: brand,
+      price: price,
+      alcoholContent: alcoholContent,
+      pintsRemaining: pintsRemaining,
+      id: id
+    };
+    expect(kegListReducer({}, action)).toEqual({
+      [id]: {
+        name: name,
+        brand: brand,
+        price: price,
+        alcoholContent: alcoholContent,
+        pintsRemaining: pintsRemaining,
+        id: id
+      }
+    });
+  });
+  
 });
