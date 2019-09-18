35be9cc6032c7a34d6ab62eb1c7ad5233d6aff8b
Student <example@example.com>
2019-09-18 09:48:24 -0700

    add reducer logic for new keg

--- src/reducers/keg-list-reducer.js
+++ src/reducers/keg-list-reducer.js
@@ -1,3 +1,19 @@
 export default (state = {}, action) => {
-  return state;
+  switch (action.type) {
+    case 'ADD_KEG':
+      const { name, brand, price, alcoholContent, pintsRemaining, id } = action;
+      let newState = Object.assign({}, state, {
+        [id]: {
+          name: name,
+          brand: brand,
+          price: price,
+          alcoholContent: alcoholContent,
+          pintsRemaining: pintsRemaining,
+          id: id
+        }
+      });
+      return newState;
+    default:
+      return state;
+  }
 };
