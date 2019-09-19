e50005c0f8c163210a67c8207d3561a153b12639
rochellev <rochellevroberts@utexas.edu>
2019-09-19 10:12:12 -0700

    combine selected keg and keg list reducers

---
+++ src/reducers/index.js
@@ -0,0 +1,11 @@
+import selectedKegReducer from './selected-keg-reducer';
+import kegListReducer from './keg-list-reducer';
+import { combineReducers } from 'redux';
+
+// structure state in two slices
+const rootReducer = combineReducers({
+    selectedKeg: selectedKegReducer,
+    masterKegList: kegListReducer
+});
+
+export default rootReducer;
