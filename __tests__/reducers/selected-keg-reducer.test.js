436c47a4439e862ec706d580f15beb910e0ca7d9
rochellev <rochellevroberts@utexas.edu>
2019-09-19 08:53:24 -0700

    selected-keg-reducer test fails

--- __tests__/reducers/selected-keg-reducer.test.js
+++ __tests__/reducers/selected-keg-reducer.test.js
@@ -1,1 +1,7 @@
 import selectedKegReducer from '../../src/reducers/selected-keg-reducer';
+
+describe("selectedKegReducer", () => {
+    test('Should return default state if no action type is recognized', () => {
+        expect(selectedKegReducer({}, {type: null})).toEqual({});
+    });
+});
