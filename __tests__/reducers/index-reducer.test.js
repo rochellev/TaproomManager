e1f00850187cbe0812c1bef55365a5c364fc8dac
rochellev <rochellevroberts@utexas.edu>
2019-09-19 10:19:02 -0700

    add test to comfirm correct reducer logic returned

--- __tests__/reducers/index-reducer.test.js
+++ __tests__/reducers/index-reducer.test.js
@@ -1,4 +1,9 @@
 import rootReducer from '../../src/reducers/index';
+import kegListReducer from '../../src/reducers/keg-list-reducer';
+import selectedKegReducer from '../../src/reducers/selected-keg-reducer';
+import {createStore} from 'redux';
+
+let store = createStore(rootReducer);
 
 describe("rootReducer", () => {
     test('Should return default state if no action type is recognized', () => {
@@ -6,5 +11,13 @@ describe("rootReducer", () => {
             masterKegList: {},
             selectedKeg: {}
         });
+    });
+
+    test('Should contain kegListReducer logic', () => {
+        expect(store.getState().selectedKeg).toEqual(kegListReducer(undefined, {type: null}));
+    });
+
+    test('Should contain selectedKegReducer logic', ()=> {
+        expect(store.getState().selectedKeg).toEqual(selectedKegReducer(undefined, {type: null}));
     });
 });
